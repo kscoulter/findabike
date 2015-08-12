0b2c5621191e1acce3cd31c98f71338ed84d8022
Cheikh Ndiongue <Cmndiongue@yahoo.com>
2015-08-12 16:40:55 -0400

    added some bike functionality

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -2,4 +2,35 @@ class BikesController < ApplicationController
   def home
   end
 
+  def index
+    @user = User.find(params[:user_id])
+    if @user.id == current_user.id
+      #show list of bikes
+    else
+      redirect_to root_path
+    end
+  end
+
+
+  def new
+  end
+
+
+  def create
+  end
+
+  def show
+    @user = current_user
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
 end
